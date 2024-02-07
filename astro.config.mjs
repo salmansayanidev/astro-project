16459c5b44d4a1cedbffd53cf6ff0737e2f30165
salmansayani <salmansayani822@gmail.com>
2024-02-08 03:20:38 +0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig, squooshImageService } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+
+    image: {
+        service: squooshImageService()
+      },
+});
